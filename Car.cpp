eaf289b079cbb33c84a2f0a5daaab9e607142423
NguyenManh1006 <manh100606@gmail.com>
2025-10-01 23:25:47 +0700

    sửa lỗi

--- Car.cpp
+++ Car.cpp
@@ -4,3 +4,20 @@ Car::Car(SDL_Texture* tex, int x, int y, int w, int h) {
     texture = tex;
     rect = {x, y, w, h};
 }
+
+void Car::Render(SDL_Renderer* renderer) {
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+}
+
+void Car::MoveDown(int speed) {
+    rect.y += speed;
+}
+
+void Car::GetRect() const {
+    return rect;
+}
+
+void Car::SetRect(int x, int y) {
+    rect.x = x;
+    rect.y = y;
+}
